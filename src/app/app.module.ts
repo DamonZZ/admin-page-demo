beb061ab1c016e5d48fad3546a59a338c3fea573
damon <zjsgdtcm@sina.com>
2018-06-26 23:16:04 +0800

    init the layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { RightNavComponent } from './right-nav/right-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    LeftNavComponent,
+    RightNavComponent
   ],
   imports: [
     BrowserModule
